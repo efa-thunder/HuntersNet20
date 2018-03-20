6f4984ecec31b70497354f50c0aad8d93a045a0e
Egor Aref'ev <efa.thunder@gmail.com>
2018-03-20 18:13:07 +0400

    After applying this commit, we add to the project reference to the Firebase Database
    Updated build.gradle for application level
    Added some code to the CreateAccount Activity

--- app/src/main/java/com/simbirsoft/itplace/efa/huntersnet/CreateAccountActivity.kt
+++ app/src/main/java/com/simbirsoft/itplace/efa/huntersnet/CreateAccountActivity.kt
@@ -1,10 +1,28 @@
 package com.simbirsoft.itplace.efa.huntersnet
 
+import android.app.ProgressDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.widget.Button
+import android.widget.ImageView
+import com.google.firebase.auth.FirebaseAuth
 import com.simbirsoft.itplace.efa.huntersnet.utilities.logd
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    //Определим переменные для UI-элементов
+    private var etEmail: TextInputEditText? = null
+    private var etPassword: TextInputEditText? = null
+    private var etDisplayName: TextInputEditText? = null
+    private var btnCreateAccount: Button? = null
+    private var ivArrowBack: ImageView? = null
+    private var mProgressBar: ProgressDialog? = null
+
+    //Определим ссылки на Firebase Authorization и Database
+    private var mAuth: FirebaseAuth? = null
+    // private var mDatabase: FirebaseDatabase? = null
+    // private var mDatabaseReference: DatabaseReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
