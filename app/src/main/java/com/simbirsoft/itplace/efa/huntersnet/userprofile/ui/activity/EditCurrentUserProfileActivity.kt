ed3195dfac145a3414622c11d7c6a79d64647e6c
Greg0ry <efa.thunder@gmail.com>
2018-04-02 22:55:37 +0400

    Delete unused import

--- app/src/main/java/com/simbirsoft/itplace/efa/huntersnet/userprofile/ui/activity/EditCurrentUserProfileActivity.kt
+++ app/src/main/java/com/simbirsoft/itplace/efa/huntersnet/userprofile/ui/activity/EditCurrentUserProfileActivity.kt
@@ -2,7 +2,6 @@ package com.simbirsoft.itplace.efa.huntersnet.userprofile.ui.activity
 
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
